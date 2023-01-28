ff15e81324f997b7d39d773e0479b92591de2618
trungdv <trungdv@hitexglobal.com>
2023-01-28 12:38:50 +0700

    update add

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+export default function App() {
+  return (
+      <WebView source={{ uri: 'http://192.168.0.104:3000' }} />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
